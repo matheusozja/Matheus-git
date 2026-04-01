c1fa4049335ff2540643378acc48b2cddcf35bf7
Matheus Henrique Barbosa da Silva <972520934@ulife.com.br>
2026-03-31 21:48:20 -0300

    Enviar aula

---
+++ Matheus/src/DeclaracaoVariáveis.java
@@ -0,0 +1,13 @@
+public class DeclaracaoVariáveis {
+    public static void main(String[] args) {
+        int numeroUm = 20; // numero inteiro
+        float numeroDois = 15.5f; // numero reais
+        String nome = "Matheus"; // conjunto de caractere
+        boolean vf =   true; // ou false
+        System.out.println("Este é meu número inteiro: "+ numeroUm);
+        System.out.println("Este é meu número real: "+ numeroDois);
+        System.out.println("Este é meu nome: "+ nome);
+        System.out.println("Este é meu valor lógico: "+ vf);
+
+    }
+}
---
+++ Matheus/src/OperacoesMatemáticas.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class OperacoesMatemáticas {
+    public static void main(String[] args) {
+        double numeroUm, NumeroDois, resultado;
+     //   int numeroUm = 2;
+        // double numeroDois = 15.5875;
+        Scanner leia = new Scanner(System.in);
+        System.out.println("Digite o primeiro número: ");
+        numeroUm = Double.parseDouble(String s) double leia.next(()
+        double resultadoSoma = numeroUm + numeroDois;
+        double resultadoMulti = numeroUm * numeroDois;
+        double resultadoDivisao = numeroUm / numeroDois;
+        double resultadoSub = numeroUm - numeroDois;
+        System.out.println("A soma é: "+ resultadoSoma);
+        System.out.println("A subtração é: "+ resultadoSub);
+        System.out.println("A divisão é: "+ resultadoDivisao);
+        System.out.println("A multiplicação é: "+ resultadoMulti);
+
+    }
+}
